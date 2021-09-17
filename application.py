b882d1c994cd7e92e53df285822f03e049e07171
philipjebaseelan <philipjebaseelan@mcruncher.com>
2021-09-17 17:28:04 +0800

    Connected SQLite DB to application

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 
+#Initialising the application & SQLite Database
 app = Flask(__name__)
+db = SQL("sqlite:///attendance.db")
 
 @app.route("/")
 def index():
