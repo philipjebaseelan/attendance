017ac894b918413e6db3050051a1bb65887ececf
philipjebaseelan <philipjebaseelan@mcruncher.com>
2021-09-17 23:34:38 +0800

    added the registration template

--- application.py
+++ application.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+
+from cs50 import SQL
+from flask import Flask, render_template, redirect, request, session
 
 #Initialising the application & SQLite Database
 app = Flask(__name__)
@@ -7,3 +9,11 @@ db = SQL("sqlite:///attendance.db")
 @app.route("/")
 def index():
     return render_template("layout.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        return redirect("/")
+    else:
+        return render_template("register.html")
